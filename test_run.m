e1b46b82af673a6b94d2183f758e966e8390a258
Massimiliano Fasi <massimiliano.fasi@manchester.ac.uk>
2021-10-07 08:07:24 +0100

    Switch from class-based to function-based unit tests

--- test_run.m
+++ test_run.m
@@ -1,4 +1,4 @@
-import matlab.unittest.TestSuite
-rsvdfastTests = matlab.unittest.TestSuite.fromClass(?RandsvdfastTest);
-result = run(rsvdfastTests);
-table(result)
+warning('off', 'randsvd_unit_tests:invalidUsage');
+test_results = run(randsvdfast_unit_tests);
+warning('on', 'randsvd_unit_tests:invalidUsage');
+table(test_results)
